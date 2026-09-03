891aadf1d513bc99d76855c32737ae5acb73af92
SameedAdeel <sameedadeel@gmail.com>
2026-09-03 15:11:17 +0500

    feat: initial commit

---
+++ src/main/java/com/example/MortartecProject/MortartecProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.MortartecProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortartecProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortartecProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MortartecProject/MortartecProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MortartecProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortartecProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
